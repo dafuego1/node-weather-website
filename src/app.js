a2018281d9b20d30c0265a94542ae66b83d4bd3a
Diego de la Rosa <diegodlrosa1234@gmail.com>
2020-10-05 21:07:29 -0500

    small change

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
-const geocode = require('../utils/geocode')
-const forecast = require('../utils/forecast')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 const app = express()
 
 // define paths for express config
