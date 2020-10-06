d0b05e0c88a54b80dc73ade62942c22d3f32a6e0
Diego de la Rosa <diegodlrosa1234@gmail.com>
2020-10-05 21:58:03 -0500

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const express = require('express')
 const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+
 const app = express()
+const port = process.env.PORT || 3000
 
 // define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -85,6 +87,7 @@ app.get('/help/*', (req,res) => {
         name: 'Diego de la Rosa'
     })
 })
+
 app.get('*', (req, res) => {
     res.render('404', {
         errorMessage: 'Page not found.',
@@ -92,6 +95,7 @@ app.get('*', (req, res) => {
         name: 'Diego de la Rosa'
     })
 })
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
